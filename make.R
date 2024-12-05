45cb45ea566e6b41684d7e94c82c861b4d877cbb
Miléna Chateau <milena.chateau@hotmail.fr>
2024-12-05 14:30:01 +0100

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' mariage: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Miléna Chateau \email{milena.chateau@imbe.fr}
+#' 
+#' @date 2024/12/05
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
